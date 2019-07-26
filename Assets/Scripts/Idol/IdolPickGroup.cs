290617be764038d2b2ed59e0d5250085470a1911
KYHSGeekCode <jourhyang123@gmail.com>
2019-07-26 18:19:41 +0900

    ApplyPersonality 처음 구현

--- Assets/Scripts/Idol/IdolPickGroup.cs
+++ Assets/Scripts/Idol/IdolPickGroup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Song;
 using Ingame;
+using System;
+using System.Linq;
 
 namespace Idol
 {
@@ -79,12 +81,105 @@ namespace Idol
 
         public float CalculateAppeal(WorkData work)
         {
+
             return 0;
         }
 
         public float ApplyPersonality(float allAppeal, float[] idolAppeal)
         {
-            return 0;
+            float[] bonus = new float[idolAppeal.Length];
+            bool applyBold = false;
+            for (int i=0; i<idolAppeal.Length;i++)
+            {
+                var idol = IngameManager.Instance.Data.Idols[IdolIndices[i]];
+                var personality = idol.Personality;
+                if(personality == IdolPersonality.Naive)
+                {
+                    if(i!=0)
+                    {
+                        var idolleft = IngameManager.Instance.Data.Idols[IdolIndices[i-1]];
+                        if(idolleft.Personality != IdolPersonality.Naive)
+                            idolleft.Personality = IdolPersonality.Unknown;
+                    }
+                    if (i!=idolAppeal.Length -1)
+                    {
+                        var idolright = IngameManager.Instance.Data.Idols[IdolIndices[i + 1]];
+                        if (idolright.Personality != IdolPersonality.Naive)
+                            idolright.Personality = IdolPersonality.Unknown;
+                    }
+                }
+            }
+            int[] counts = new int[Enum.GetNames(typeof(IdolPersonality)).Length];
+            for(int i=0; i<idolAppeal.Length;i++)
+            {
+                var idol = IngameManager.Instance.Data.Idols[IdolIndices[i]];
+                var personality = idol.Personality;
+                counts[(int)personality]++;
+            }
+            counts[(int)IdolPersonality.Unknown] = 0;
+
+            //count personality 종류
+            int percnt = 0;
+            for(int i=0; i< counts.Length;i++)
+            {
+                if (counts[i] > 0)
+                    percnt++;
+            }
+            for (int i=0;i<idolAppeal.Length;i++)
+            {
+                var idol = IngameManager.Instance.Data.Idols[IdolIndices[i]];
+                var personality = idol.Personality;
+                
+                switch (personality)
+                {
+                    case IdolPersonality.Unknown:
+                        break;
+                    case IdolPersonality.Bashful:       //만약 아이돌이 자기 자신뿐이라면 총 어필 +10% 같이 참여하는 아이돌당 자신의 어필 - 5 %
+                        if(idolAppeal.Length ==1)
+                        {
+                            bonus[i] = 1.1f;
+                        } else
+                        {
+                            bonus[i] = Math.Max(1.0f - 0.05f * idolAppeal.Length, 0.3f);
+                        }
+                        break;
+                    case IdolPersonality.Jolly:         //같이 참여하는 명랑함 아이돌당 자신의 어필 +5%
+                        bonus[i] = 1f + counts[(int)IdolPersonality.Jolly] * 0.05f;
+                        break;
+                    case IdolPersonality.Naive:         //양 옆 슬롯 아이돌의 성격 보정 효과를 제거
+                        // pre-processed
+                        break;
+                    case IdolPersonality.Quirky:        //자신의 어필 랜덤으로 -5% ~ 5%
+                        bonus[i] = 1.0f + 0.01f * new System.Random().Next(-5, 6);
+                        break;
+                    case IdolPersonality.Bold:          //총 어필 +10% (여러 명이 있어도 한 번만 적용)
+                        applyBold = true;
+                        break;
+                    case IdolPersonality.Mild:          //같이 참여하는 아이돌의 성격 하나당 자신의 어필 +2%
+                        bonus[i] = 1.0f + 0.02f * percnt;
+                        break;
+                    case IdolPersonality.Relaxed:       //만약 무사태평함 아이돌이 자기 자신뿐이라면 자신의 어필 +10%
+                        if(counts[(int)IdolPersonality.Relaxed]==1)
+                        {
+                            bonus[i] = 1.1f;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            for(int i=0;i<idolAppeal.Length;i++)
+            {
+                idolAppeal[i] *= bonus[i];
+            }
+            if (applyBold)
+            {
+                for (int i = 0; i < idolAppeal.Length; i++)
+                {
+                    idolAppeal[i] *= 1.1f;
+                }
+            }
+            return idolAppeal.Sum();
         }
     }
 }
